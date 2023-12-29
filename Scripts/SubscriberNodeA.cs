3b71c448fc1052fe2a7a58a22e045349fd713a28
abelbascu <abel.bascu@gmail.com>
2023-12-29 23:19:59 +0100

    SubscriberNodeA.cs shows how to get nodes that may not be initialized yet

--- Scripts/SubscriberNodeA.cs
+++ Scripts/SubscriberNodeA.cs
@@ -13,8 +13,10 @@ public partial class SubscriberNodeA : CharacterBody2D
     }
 
     public async void OnNodeReady() {
+        
         //WAYS TO GET A NODE IN THE SCENE THAT MAY NOT BE READY YET
         //OPTION 1:
+
         do {
             emitterNode = GetNode<EmitterNode>("../EmitterNode");
         } while (!GetNode<EmitterNode>("../EmitterNode").IsInsideTree());
@@ -22,20 +24,23 @@ public partial class SubscriberNodeA : CharacterBody2D
         //OPTION 2.
         //"../EmitterNode" means go up one level in the hierarchy   
         //you can also use "/root/MainScene/EmitterNode", this is the absolute path
-        if (GetNode<EmitterNode>("../EmitterNode") == null) {   
+
+        if (GetNode<EmitterNode>("../EmitterNode") == null) {
             timer.Start(0.5);
             await ToSignal(timer, "timeout");
-            await ToSignal(GetNode<EmitterNode>("EmitterNode"), GetNode<EmitterNode>("EmitterNode").SignalName.Ready);
         }
+
         //after the timeout, we continue. We have waited 0.5secs for the target node to be ready,
         //seems enough but the line below is dangerous as it could fail
-        emitterNode = GetNode<EmitterNode>("../EmitterNode"); 
+
+        emitterNode = GetNode<EmitterNode>("../EmitterNode");
         emitterNode.StealsAppleFromGrocery += OnKiddoStealsApple;
     }
 
     public void OnKiddoStealsApple() {
         GetNode<Label>("AppleStolenAnswer").Text = ("You kiddo! I saw what you did. Leave the apple where it was!");
     }
-
-
 }
+
+
+//await ToSignal(GetNode<EmitterNode>("EmitterNode"), GetNode<EmitterNode>("EmitterNode").SignalName.Ready);
